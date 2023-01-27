c4842bb8c6693e456b6c6f325cc0c3d7cd24e0eb
DalessandroFX <114603690+DalessandroFX@users.noreply.github.com>
2023-01-27 00:52:07 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Frase from './components/Frase';
+import IssoEUmaProps from './components/IssoEUmaProps';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dalessandro Ferreira Xavier</h1>
+      <Frase/>
+      <IssoEUmaProps name="dalessandro" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+        <div>
+            <h1>esse é um componente</h1>
+        </div>
+    )
+}
+
+export default Frase
